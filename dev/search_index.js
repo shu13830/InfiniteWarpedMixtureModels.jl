e2b326ca1f2c7d4217be5dfae536afa98d976636
Documenter.jl <documenter@juliadocs.github.io>
2022-01-23 08:43:39 +0000

    build based on 77eeec8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InfiniteWarpedMixtureModels","category":"page"},{"location":"#InfiniteWarpedMixtureModels","page":"Home","title":"InfiniteWarpedMixtureModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InfiniteWarpedMixtureModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InfiniteWarpedMixtureModels]","category":"page"}]
+}
